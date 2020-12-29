7c288e88f73be06ac0a1e1261ff03315dcfdfb8a
httpdaniel <daniel.097@hotmail.com>
2020-12-29 15:17:25 +0000

    Updating transform_data

--- transform_data.py
+++ transform_data.py
@@ -1,1 +1,16 @@
-# TODO encode the data
+# import packages
+import pandas as pd
+
+# read in csv
+reviews = pd.read_csv('data/preprocessed_reviews.csv')
+
+# add length column
+reviews['Review'] = reviews['Review'].astype(str)
+reviews['Length'] = reviews['Review'].apply(lambda x: len(x.split()))
+
+# encode recommended & early access
+encoded = pd.get_dummies(reviews, prefix=['Recommended', 'Early Access'],
+                         columns=['Recommended', 'Early Access'], prefix_sep=" ")
+
+# save to csv
+encoded.to_csv('data/transformed_reviews.csv', index=False, encoding='utf-8')
