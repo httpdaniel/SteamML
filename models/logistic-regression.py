fc2220fff4e980a1aab11492e1d90c66314820b6
httpdaniel <daniel.097@hotmail.com>
2020-12-31 16:00:27 +0000

    Updating logistic regression

--- models/logistic-regression.py
+++ models/logistic-regression.py
@@ -2,16 +2,18 @@
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score
+from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score, recall_score, precision_score
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sn
 
 
 # split data and vectorize review text
-def get_data(csv, vect):
+def get_data(csv, vect, feature, target):
     reviews = pd.read_csv(csv)
 
-    review_text = reviews['Review'].values.astype('U')
-    recommended = reviews['Recommended']
+    review_text = reviews[feature].values.astype('U')
+    recommended = reviews[target]
 
     xtrain, xtest, ytrain, ytest = train_test_split(review_text, recommended, test_size=0.3, random_state=42)
 
@@ -21,10 +23,53 @@ def get_data(csv, vect):
     return xtrain, xtest, ytrain, ytest
 
 
-vectorizer = CountVectorizer()
-# vectorizer = TfidfVectorizer(min_df=5, ngram_range=[1, 3])
+def evaluate(real, pred):
+    # compute auc score
+    aucscore = roc_auc_score(real, pred)
+    print("AUC Score: ", aucscore, "\n")
 
-X_train, X_test, y_train, y_test = get_data('../data/transformed_reviews.csv', vectorizer)
+    # compute accuracy score
+    accscore = accuracy_score(real, pred)
+    print("Accuracy Score: ", accscore, "\n")
+
+    # recall
+    recall = recall_score(real, pred, average=None)
+    print("Recall: ", recall, "\n")
+
+    # precision
+    precision = precision_score(real, pred, average=None, zero_division=0)
+    print("Precision: ", precision, "\n")
+
+    # find values for confusion matrix
+    tn, fp, fn, tp = confusion_matrix(real, pred).ravel()
+    print("--- Confusion Matrix--\n", tn, fp, fn, tp)
+
+    array = [[tp, fp], [fn, tn]]
+
+    df_cm = pd.DataFrame(array, index=["Pred Recommended", "Pred Not-Recommended"],
+                         columns=["Act Recommended", "Act not-Recommended"])
+
+    fig, ax = plt.subplots(figsize=(10, 7))
+    sn.heatmap(df_cm, annot=True, fmt='')
+
+    ax.set_title("Logistic Regression - Confusion Matrix\n")
+    ax.xaxis.tick_top()
+    plt.show()
+
+    print("\nTrue Positives:", tp)
+    print("False Positives ", fp)
+    print("True Negatives: ", tn)
+    print("False Negatives: ", fn)
+
+    print("True Positive Rate: ", (tp / (tp + fn)))
+    print("Specificity: ", (tn / (tn + fp)))
+    print("False Positive Rate: ", (fp / (fp + tn)))
+
+
+# vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer(min_df=5, ngram_range=[1, 3])
+
+X_train, X_test, y_train, y_test = get_data('../data/transformed_reviews.csv', vectorizer, 'Review', 'Recommended')
 
 # fit model
 model = LogisticRegression(max_iter=10000)
@@ -33,24 +78,5 @@ model.fit(X_train, y_train)
 # make predictions
 predictions = model.predict(vectorizer.transform(X_test))
 
-# compute auc score
-aucscore = roc_auc_score(y_test, predictions)
-print("AUC Score: ", aucscore, "\n")
-
-# compute accuracy score
-accscore = accuracy_score(y_test, predictions)
-print("Accuracy Score: ", accscore, "\n")
-
-tp, fp, fn, tn = confusion_matrix(y_test, predictions).ravel()
-print(pd.DataFrame(confusion_matrix(y_test, predictions),
-                   columns=['Actual Recommended', "Actual Not-Recommended"],
-                   index=['Predicted Recommended', 'Predicted Not-Recommended']))
-
-print(f'\nTrue Positives: {tp}')
-print(f'False Positives: {fp}')
-print(f'True Negatives: {tn}')
-print(f'False Negatives: {fn}')
-
-print(f'True Positive Rate: { (tp / (tp + fn))}')
-print(f'Specificity: { (tn / (tn + fp))}')
-print(f'False Positive Rate: { (fp / (fp + tn))}')
+# evaluate results
+evaluate(y_test, predictions)
