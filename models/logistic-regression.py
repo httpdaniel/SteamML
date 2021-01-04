72b286f8459331332c4fe47d5fee6d0d967a23bb
httpdaniel <daniel.097@hotmail.com>
2021-01-04 20:14:27 +0000

    Updating logistic-regression

--- models/logistic-regression.py
+++ models/logistic-regression.py
@@ -3,24 +3,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score, recall_score, precision_score
+from scipy.sparse import csr_matrix, hstack
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sn
 
 
 # split data and vectorize review text
-def get_data(csv, vect, feature, target):
+def get_data(csv, vect, target):
     reviews = pd.read_csv(csv)
 
-    review_text = reviews[feature].values.astype('U')
-    recommended = reviews[target]
+    reviews['Review'] = reviews['Review'].values.astype('U')
+    Y = reviews[target]
+    X = reviews.drop(['Recommended', 'Early Access'], axis=1)
 
-    xtrain, xtest, ytrain, ytest = train_test_split(review_text, recommended, test_size=0.3, random_state=42)
+    xtrain, xtest, ytrain, ytest = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-    vect.fit(xtrain)
-    xtrain = vect.transform(xtrain)
+    xtrain_t = vect.fit_transform(xtrain.Review)
+    xtrain_final = hstack([xtrain_t, csr_matrix(xtrain.Length).T], 'csr')
 
-    return xtrain, xtest, ytrain, ytest
+    xtest_t = vect.transform(xtest.Review)
+    xtest_final = hstack([xtest_t, csr_matrix(xtest.Length).T], 'csr')
+
+    return xtrain_final, xtest_final, ytrain, ytest
 
 
 def evaluate(real, pred):
@@ -69,14 +74,14 @@ def evaluate(real, pred):
 # vectorizer = CountVectorizer()
 vectorizer = TfidfVectorizer(min_df=5, ngram_range=[1, 3])
 
-X_train, X_test, y_train, y_test = get_data('../data/transformed_reviews.csv', vectorizer, 'Review', 'Recommended')
+X_train, X_test, y_train, y_test = get_data('../data/transformed_reviews.csv', vectorizer, 'Recommended')
 
 # fit model
 model = LogisticRegression(max_iter=10000)
 model.fit(X_train, y_train)
 
 # make predictions
-predictions = model.predict(vectorizer.transform(X_test))
+predictions = model.predict(X_test)
 
 # evaluate results
 evaluate(y_test, predictions)
